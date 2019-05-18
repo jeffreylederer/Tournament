4d677302a090a150bb4ea1b28572e43abe83593f
jeff <jeffrey@winnlederer.com>
2019-05-18 13:27:27 -0400

    added players

---
+++ Tournament/Controllers/PlayersController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Elmah;
+using Tournament.Models;
+
+namespace Tournament.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private TournamentEntities db = new TournamentEntities();
+
+        // GET: Players
+        public ActionResult Index(string sortOrder)
+        {
+            ViewData["FullNameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["FirstNameSortParm"] = sortOrder == "firstname" ? "firstname_desc" : "firstname";
+            var list = from s in db.Players
+                select s;
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    list = list.OrderByDescending(s => s.LastName + " " + s.FirstName);
+                    break;
+                case "firstname_desc":
+                    list = list.OrderBy(s => s.FirstName);
+                    break;
+                case "firstname":
+                    list = list.OrderByDescending(s => s.FirstName);
+                    break;
+                default:
+                    list = list.OrderBy(s => s.LastName + " " + s.FirstName);
+                    break;
+            }
+            ViewBag.Count = list.Count();
+            ViewBag.Active = db.Players.Where(x=>x.Active).Count();
+
+            return View(list);
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            var item = new Player()
+            {
+                Active = true
+            };
+            return View(item);
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,FirstName,LastName,Active,FullName,shortname,NickName")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(player);
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    Exception ex = e;
+                    while (ex.InnerException != null)
+                        ex = ex.InnerException;
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    ModelState.AddModelError(string.Empty, "Insert failed");
+                }
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,FirstName,LastName,Active,FullName,shortname,NickName")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+                {
+                    Exception ex = e;
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    while (ex.InnerException != null)
+                        ex = ex.InnerException;
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    ModelState.AddModelError(string.Empty, "Edit failed");
+                }
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Players.Find(id);
+            db.Players.Remove(player);
+            try
+            {
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+            {
+                Exception ex = e;
+                ErrorSignal.FromCurrentContext().Raise(e);
+                while (ex.InnerException != null)
+                    ex = ex.InnerException;
+                ViewBag.Error = ex.Message;
+            }
+            catch (Exception e)
+            {
+                ErrorSignal.FromCurrentContext().Raise(e);
+                ViewBag.Error = "Delete failed";
+            }
+            return View(player);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
