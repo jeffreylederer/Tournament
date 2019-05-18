815dd610242c3d3460bd02540b9c8d07d3b74cfc
jeff <jeffrey@winnlederer.com>
2019-05-18 17:30:38 -0400

    added schedule

---
+++ Tournament/Controllers/SchedulesController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Elmah;
+using Tournament.Models;
+
+namespace Tournament.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private TournamentEntities db = new TournamentEntities();
+
+        // GET: Schedules
+        public ActionResult Index()
+        {
+            return View(db.Schedules.ToList());
+        }
+
+        // GET: Schedules/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,RoundNo")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Schedules.Add(schedule);
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    Exception ex = e;
+                    while (ex.InnerException != null)
+                        ex = ex.InnerException;
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    ModelState.AddModelError(string.Empty, "Insert failed");
+                }
+            }
+
+            return View(schedule);
+        }
+
+        // GET: Schedules/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,RoundNo")] Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(schedule).State = EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    Exception ex = e;
+                    while (ex.InnerException != null)
+                        ex = ex.InnerException;
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                catch (Exception e)
+                {
+                    ErrorSignal.FromCurrentContext().Raise(e);
+                    ModelState.AddModelError(string.Empty, "Edit failed");
+                }
+            }
+            return View(schedule);
+        }
+
+        // GET: Schedules/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Schedule schedule = db.Schedules.Find(id);
+            if (schedule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedule);
+        }
+
+        // POST: Schedules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Schedule schedule = db.Schedules.Find(id);
+            db.Schedules.Remove(schedule);
+            try
+            {
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+            {
+                ErrorSignal.FromCurrentContext().Raise(e);
+                Exception ex = e;
+                while (ex.InnerException != null)
+                    ex = ex.InnerException;
+                ViewBag.Error = ex.Message;
+            }
+            catch (Exception e)
+            {
+                ErrorSignal.FromCurrentContext().Raise(e);
+                ViewBag.Error = "Delete failed";
+            }
+            return View(schedule);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tournament/Models/MetaData.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tournament.Models
+{
+    [MetadataType(typeof(PlayerMetaData))]
+    public partial class Player
+    {
+    }
+    public class PlayerMetaData
+    {
+
+        [Display(Name = "First Name")]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Shorten Name")]
+        [StringLength(25)]
+        public string shortname { get; set; }
+
+        [Display(Name = "In Tuesday League")]
+        public bool TuesdayLeague { get; set; }
+        [Display(Name = "In Wednesday League")]
+        public bool WednesdayLeague { get; set; }
+
+        [Display(Name = "Player")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Nickname")]
+        public string NickName { get; set; }
+    }
+
+    [MetadataType(typeof(TeamMetaData))]
+    public partial class Team
+    {
+    }
+    public partial class TeamMetaData
+    {
+        [Display(Name = "Team Number")]
+        [Required]
+        public int id { get; set; }
+
+        [Display(Name = "Vice Skip")]
+        public string ViceSkip { get; set; }
+    }
+
+
+    [MetadataType(typeof(ScheduleMetaData))]
+    public partial class Schedule
+    {
+    }
+    public partial class ScheduleMetaData
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Display(Name = "Round Number")]
+        public int RoundNo { get; set; }
+    }
+}
