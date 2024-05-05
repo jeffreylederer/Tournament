7371d2dde7ff01189ddae80c04f2957c3465422d
Jeffrey Lederer <jeffrey@winnlederer.com>
2024-05-05 11:43:54 -0400

    added database definition

---
+++ TournamentDB/dbo/Tables/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[User] (
+    [id]         INT           IDENTITY (1, 1) NOT NULL,
+    [username]   VARCHAR (200) NOT NULL,
+    [password]   VARCHAR (100) NOT NULL,
+    [Roles]      VARCHAR (50)  NULL,
+    [rowversion] ROWVERSION    NOT NULL,
+    CONSTRAINT [PK_User_1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_User]
+    ON [dbo].[User]([username] ASC);
+
